c0a994f382e5ce52985e99ea179b6860a5275898
juhyg-hash <gusrb1474@gmail.com>
2025-06-16 11:01:25 +0900

    second commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+class Car {
+    String brand;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    class Engine {
+        public void start() {
+            System.out.println("The engine of " + brand + " is starting.");
+        }
+    }
+
+    public void startEngine() {
+        Engine engien = new Engine();
+        engien.start();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("빠른자동차");
+
+        car.startEngine();
+    }
+}
