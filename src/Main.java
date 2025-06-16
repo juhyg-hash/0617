24ce88e11f2a52749e8ce3d8c6b0079a508a8a29
juhyg-hash <gusrb1474@gmail.com>
2025-06-16 13:17:15 +0900

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,19 @@
-class Outer{
-    String outm = "오이시";
-    public class Iner {
-        void showMessage() {
-            System.out.println(outm);
+class Outer {
+    private class HiddenInner {
+        void secret() {
+            System.out.println("private!");
         }
     }
 
+    public void reveal() {
+        HiddenInner hi = new HiddenInner();
+        hi.secret();
+    }
 }
+
 public class Main {
-    String outerMessage = "Hello from Outer";
-
-    class Inner {
-        void showMessage() {
-            System.out.println(outerMessage);
-        }
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
-
-        Main.Inner inner = main.new Inner();
-
-        inner.showMessage();
-
         Outer outer = new Outer();
-
-        Outer.Iner inn = outer.new Iner();
 
     }
 }
